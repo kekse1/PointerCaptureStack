dbbbfd274d8fa2c3fca28b65afb42e4551fd2fc1
Sebastian 'Kuchen' Kucharczyk <kuchen@kekse.biz>
2026-01-28 13:46:50 +0100

    1769604409459409551

---
+++ README.md
@@ -0,0 +1,26 @@
+<img src="https://kekse.biz/github.php?draw&override=github:pointercapturestack" />
+
+<br>
+
+# Pointer Capture **Stack**
+...
+
+This README is still TODO!
+
+<br><br>
+
+## Download
+* [`capture.js`](./src/capture.js) (created **2026-01-28**);
+
+# Contact
+
+<img src="https://kekse.biz/github.php?override=github:pointercapturestack&draw&text=pointercapturestack@kekse.biz&angle=6&size=24px&fg=150,20,90&font=OpenSans&ro&readonly&h=64&v=16" />
+
+# Copyright and License
+The Copyright is [(c) Sebastian Kucharczyk](./COPYRIGHT.txt),
+and it's licensed under the [MIT](./LICENSE.txt) (also known as 'X' or 'X11' license).
+
+<a href="https://kekse.biz/">
+<img src="favicon.png" alt="Favicon" />
+</a>
+
---
+++ src/capture.js
@@ -0,0 +1,120 @@
+/*
+* Copyright (c) Sebastian Kucharczyk <kuchen@kekse.biz>
+* https://kekse.biz/ https://github.com/kekse1/v4/
+*/
+
+//
+const capture = window.PointerCapture = {};
+capture.MAP = new Map(); export default capture;
+
+//
+var ignoreEvents = false;
+
+//
+const push = (_id, _target = null) => {
+	ignoreEvents = true;
+	var stack;
+
+	if(!capture.MAP.has(_id))
+	{
+		capture.MAP.set(_id, stack = []);
+	}
+	else
+	{
+		stack = capture.MAP.get(_id);
+	}
+
+	const prev = stack[stack.length - 1];
+
+	if(prev)
+	{
+		prev.releasePointerCapture(_id);
+	}
+
+	stack.push(_target);
+	ignoreEvents = false;
+};
+
+const pop = (_id, _target = null) => {
+	ignoreEvents = true;
+
+	if(!capture.MAP.has(_id))
+	{
+		return;
+	}
+
+	const stack = capture.MAP.get(_id);
+	var remove, prev;
+	
+	if(_target)
+	{
+		remove = stack[stack.indexOf(_target)];
+	}
+	else
+	{
+		remove = stack.pop();
+	}
+
+	if(remove)
+	{
+		remove.releasePointerCapture(_id);
+		stack.remove(remove);
+	}
+
+	do
+	{
+		try
+		{
+			prev = stack[stack.length - 1];
+			if(prev) prev.setPointerCapture(_id);
+			else break;
+		}
+		catch(_err)
+		{
+			prev = stack.pop();
+		}
+
+		if(stack.length === 0)
+		{
+			break;
+		}
+	}
+	while(true);
+	
+	if(stack.length === 0)
+	{
+		capture.MAP.delete(_id);
+	}
+
+	ignoreEvents = false;
+};
+
+//
+const onGotPointerCapture = (_event) => {
+	if(!ignoreEvents)
+	{
+		push(_event.pointerId,
+			_event.target);
+	}
+};
+
+const onLostPointerCapture = (_event) => {
+	if(!ignoreEvents)
+	{
+		pop(_event.pointerId,
+			_event.target);
+	}
+};
+
+//
+setTimeout(() => {
+	window.on('gotpointercapture',
+		onGotPointerCapture,
+			{ passive: true });
+	window.on('lostpointercapture',
+		onLostPointerCapture,
+			{ passive: true });
+});
+
+//
+
